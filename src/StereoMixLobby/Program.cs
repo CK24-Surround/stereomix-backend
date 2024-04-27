b6d3d37f648a1e04e240bb7863275998233ba158
pid011 <pid011@naver.com>
2024-04-27 14:11:14 +0900

    Update launch settings

--- src/StereoMixLobby/Program.cs
+++ src/StereoMixLobby/Program.cs
@@ -10,6 +10,7 @@ WebApplication app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/",
-    () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    () => "Communication with gRPC endpoints must be made through a gRPC client.\n" +
+          "To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
