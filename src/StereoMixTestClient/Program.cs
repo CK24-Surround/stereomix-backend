d3678c86804dab4be02bfb4a727a04646dddd863
pid011 <pid011@naver.com>
2024-04-27 13:17:03 +0900

    Init project

---
+++ src/StereoMixTestClient/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
+using StereoMixLobby;
+
+Console.WriteLine("Hello, World!");
+
+if (args.Length < 1)
+{
+    Console.WriteLine("Pass address as argument.");
+    return;
+}
+
+var channel = GrpcChannel.ForAddress(args[0]);
+
+var greeter = new Greeter.GreeterClient(channel);
+
+HelloReply reply = await greeter.SayHelloAsync(new HelloRequest { Name = "StereoMixTestClient" });
+Console.WriteLine(reply.Message);
