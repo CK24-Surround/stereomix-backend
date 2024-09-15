3dffcbb380296f2ed7e6e5f13833cea41c3b1ad0
플밍 곽필경 <pid011@naver.com>
2024-09-15 14:41:14 +0900

    Database Id 명시적으로 사용하도록 적용

--- src/StereoMix/Firestore/FirestoreClient.cs
+++ src/StereoMix/Firestore/FirestoreClient.cs
@@ -10,11 +10,14 @@ public interface IFirestoreClient
 public class FirestoreClient : IFirestoreClient
 {
     private const string FirestoreProjectIdEnvName = "FIRESTORE_PROJECT_ID";
+    private const string FirestoreDatabaseIdEnvName = "FIRESTORE_DATABASE_ID";
 
     public FirestoreClient(IConfiguration configuration)
     {
         var projectId = configuration[FirestoreProjectIdEnvName] ?? throw new InvalidOperationException($"{FirestoreProjectIdEnvName} is not set");
-        Database = FirestoreDb.Create(projectId);
+        var databaseId = configuration[FirestoreDatabaseIdEnvName] ?? throw new InvalidOperationException($"{FirestoreDatabaseIdEnvName} is not set");
+        var builder = new FirestoreDbBuilder { ProjectId = projectId, DatabaseId = databaseId };
+        Database = builder.Build();
     }
 
     public FirestoreDb Database { get; }
